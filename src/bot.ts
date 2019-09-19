05da75edf939fda2ee0841e9fc1b04fc7b9f7908
Michael Szul <michael@szul.us>
2019-09-19 16:10:22 -0400

    Added logic for gathering emotion score

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,15 @@
 import { TurnContext, ActivityTypes, ConversationState } from "botbuilder";
-import { DialogSet, DialogContext, WaterfallDialog, WaterfallStepContext } from "botbuilder-dialogs";
-import { topEmotion, topEmotionResult } from "./util";
+import { DialogSet, DialogContext, WaterfallDialog, WaterfallStepContext, TextPrompt } from "botbuilder-dialogs";
+import { topEmotionScore } from "./util";
+import { EmotionScore } from "./schema";
 
 export class RogersBot {
     public state: ConversationState;
     public dialogs: DialogSet;
-    public constructor(state, dialogs) {
+    public constructor(state: ConversationState, dialogs: DialogSet) {
         this.state = state;
         this.dialogs = dialogs;
+        this.addHelloDialogs();
     }
     async onTurn(context: TurnContext): Promise<void> {
         const dialogContext: DialogContext = await this.dialogs.createContext(context);
@@ -21,5 +23,22 @@ export class RogersBot {
             }
             await context.sendActivity(`You said "${context.activity.text}"`);
         }
+    }
+    private addHelloDialogs(): void {
+        this.dialogs.add(new WaterfallDialog("hello", [
+            async (step: WaterfallStepContext) => {
+                return await step.prompt("textPrompt", "Hello. How are you doing today?");
+            },
+            async (step: WaterfallStepContext) => {
+                const emotion: EmotionScore = step.context.turnState.get("emotionDetection");
+                const emotionScore: EmotionScore = topEmotionScore(emotion);
+                switch(emotionScore.name) {
+                    default:
+                        break;
+                }
+                return await step.endDialog();
+            }
+        ]));
+        this.dialogs.add(new TextPrompt("textPrompt"));
     }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,18 @@
 import * as nlup from 'ibm-watson/natural-language-understanding/v1.js';
+import { EmotionScore } from "./schema";
 
-export function topEmotion(emotionResult: nlup.EmotionResult[]): string {
-    const sortedEmotions: string[] = Object.keys(emotionResult).sort((a: string, b: string) => emotionResult[a] - emotionResult[b]);
+export function getEmotions(result: nlup.EntitiesResult | nlup.KeywordsResult): nlup.EmotionScores {
+    if(result.emotion !== undefined) {
+        return result.emotion;
+    }
+}
+
+export function topEmotion(emotionScores: nlup.EmotionScores): string {
+    const sortedEmotions: string[] = Object.keys(emotionScores).sort((a: string, b: string) => emotionScores[a] - emotionScores[b]);
     return sortedEmotions[0];
 }
 
-export function topEmotionResult(emotionResult: nlup.EmotionResult[]): nlup.EmotionResult {
+export function topEmotionScore(emotionResult: nlup.EmotionScores): EmotionScore {
     const sortedEmotions: string[] = Object.keys(emotionResult).sort((a: string, b: string) => emotionResult[a] - emotionResult[b]);
     return emotionResult[sortedEmotions[0]];
 }
