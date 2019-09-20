09d223a4062591efa8b0b37279e60278c5aa201e
Michael Szul <michael@szul.us>
2019-09-20 15:05:09 -0400

    Started dialog flows

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,8 @@
 import { TurnContext, ActivityTypes, ConversationState } from "botbuilder";
-import { DialogSet, DialogContext, WaterfallDialog, WaterfallStepContext, TextPrompt } from "botbuilder-dialogs";
+import { DialogSet, DialogContext, WaterfallDialog, WaterfallStepContext, TextPrompt, DialogTurnResult } from "botbuilder-dialogs";
 import * as nlup from 'ibm-watson/natural-language-understanding/v1.js';
 import { topEmotionScore } from "./util";
-import { Emotion } from "./schema";
+import { Emotion, EXIT } from "./schema";
 
 export class RogersBot {
     public state: ConversationState;
@@ -27,17 +27,27 @@ export class RogersBot {
     }
     private addHelloDialogs(): void {
         this.dialogs.add(new WaterfallDialog("hello", [
-            async (step: WaterfallStepContext) => {
+            async (step: WaterfallStepContext): Promise<DialogTurnResult> => {
+                const opts = step.options;
+                if(opts["repeat"] !== undefined) {
+                    return await step.next();
+                }
                 return await step.prompt("textPrompt", "Hello. How are you doing today?");
             },
-            async (step: WaterfallStepContext) => {
+            async (step: WaterfallStepContext): Promise<DialogTurnResult> => {
+                if(EXIT.indexOf((<string>step.result).toLocaleLowerCase().trim()) !== -1) {
+                    return await step.endDialog();
+                }
                 const emotionScores: nlup.EmotionScores = step.context.turnState.get("emotionDetection");
                 const emotion: Emotion = topEmotionScore(emotionScores);
+                if(emotion.score === 0) {
+                    await step.prompt("textPrompt", "You seem uncertain. Why don't you tell me about how your morning started?");
+                }
                 switch(emotion.name) {
                     default:
                         break;
                 }
-                return await step.endDialog();
+                return await step.prompt("textPrompt", "Tell me more.");
             }
         ]));
         this.dialogs.add(new TextPrompt("textPrompt"));
--- src/schema.ts
+++ src/schema.ts
@@ -2,3 +2,11 @@ export interface Emotion {
     name: string;
     score: number;
 }
+
+export const EXIT: string[] = [
+    "goodbye",
+    "bye",
+    "im tired",
+    "i'm tired",
+    "exit"
+];
