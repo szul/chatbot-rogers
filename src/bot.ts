688bf21856b726140a2bfeb96d986c89235d4302
Michael Szul <michael@szul.us>
2019-09-21 09:17:44 -0400

    Finished dialog looping

--- src/bot.ts
+++ src/bot.ts
@@ -11,45 +11,64 @@ export class RogersBot {
         this.state = state;
         this.dialogs = dialogs;
         this.addHelloDialogs();
+        this.addPsychotherapyDialogs();
+        this.addPrompts();
     }
     async onTurn(context: TurnContext): Promise<void> {
         const dialogContext: DialogContext = await this.dialogs.createContext(context);
         await dialogContext.continueDialog();
         if (context.activity.type === ActivityTypes.Message) {
-            if (context.activity.text === "hello") {
-                await dialogContext.beginDialog("hello");
-            }
-            if (context.activity.text === "help") {
-                await dialogContext.beginDialog("help");
+            if(!context.responded) {
+                if (context.activity.text === "hello") {
+                    await dialogContext.beginDialog("hello");
+                }
+                else {
+                    await dialogContext.beginDialog("psychotherapy");
+                }
             }
         }
         await this.state.saveChanges(context);
     }
+    private handleEmotion(emotionState: any): string {
+        const emotionScores: nlup.EmotionScores = emotionState;
+        const emotion: Emotion = topEmotionScore(emotionScores);
+        if(emotion.score === 0) {
+            return "You seem uncertain. Why don't you tell me about how your morning started?";
+        }
+        switch(emotion.name) {
+            default:
+                break;
+        }
+        return null;
+    }
+    private async loopDialog(step: WaterfallStepContext): Promise<DialogTurnResult> {
+        if(step.result != null && EXIT.indexOf((<string>step.result).toLocaleLowerCase().trim()) !== -1) {
+            return await step.endDialog();
+        }
+        const response: string = this.handleEmotion(step.context.turnState.get("emotionDetection"));
+        if(response != null) {
+            return await step.prompt("textPrompt", response);
+        }
+        return await step.prompt("textPrompt", "Tell me more.");
+    }
     private addHelloDialogs(): void {
         this.dialogs.add(new WaterfallDialog("hello", [
             async (step: WaterfallStepContext): Promise<DialogTurnResult> => {
-                const opts = step.options;
-                if(opts["repeat"] !== undefined) {
-                    return await step.next();
-                }
                 return await step.prompt("textPrompt", "Hello. How are you doing today?");
             },
             async (step: WaterfallStepContext): Promise<DialogTurnResult> => {
-                if(EXIT.indexOf((<string>step.result).toLocaleLowerCase().trim()) !== -1) {
-                    return await step.endDialog();
-                }
-                const emotionScores: nlup.EmotionScores = step.context.turnState.get("emotionDetection");
-                const emotion: Emotion = topEmotionScore(emotionScores);
-                if(emotion.score === 0) {
-                    await step.prompt("textPrompt", "You seem uncertain. Why don't you tell me about how your morning started?");
-                }
-                switch(emotion.name) {
-                    default:
-                        break;
-                }
-                return await step.prompt("textPrompt", "Tell me more.");
+                return await this.loopDialog(step);
             }
         ]));
+    }
+    private addPsychotherapyDialogs(): void {
+        this.dialogs.add(new WaterfallDialog("psychotherapy", [
+            async (step: WaterfallStepContext): Promise<DialogTurnResult> => {
+                return await this.loopDialog(step);
+            }
+        ]));
+    }
+    private addPrompts(): void {
         this.dialogs.add(new TextPrompt("textPrompt"));
     }
 }
